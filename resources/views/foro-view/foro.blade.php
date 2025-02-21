591734bb08b80b566043840b1344ec08ef8b8f4a
David Ismael Espinoza <idespinoza2021@gmail.com>
2025-02-21 11:09:25 -0600

    Conexion

--- resources/views/foro-view/foro.blade.php
+++ resources/views/foro-view/foro.blade.php
@@ -173,6 +173,8 @@
         </div>
     </div>
     <br><br><br><br><br>
+    verifuicacion de conexion 
+
 @endsection
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
